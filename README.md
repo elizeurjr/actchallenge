4e20bc650ed108e312102da4c951ade7e5312281
elizeurjr <elizeuribeiroo@gmail.com>
2025-02-25 00:10:35 -0300

    feat: add new components for backend e frontend

--- README.md
+++
@@ -1,4 +0,0 @@
-# actchallenge
-repository to prepare challenges for the selection process for ACT
-
-version 1.0.1
--- tests/test_app.py
+++
@@ -1,6 +0,0 @@
-def test_hello():
-    from app import app
-    with app.test_client() as client:
-        response = client.get('/')
-        assert response.status_code == 200
-        assert response.data.decode() == 'Hello, World!'
